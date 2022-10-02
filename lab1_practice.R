7a7ec28a761b7f2774126e3c6ae6352ece6f14e0
sgaiesky <sgaiesky@gmail.com>
2022-10-01 18:02:13 -0700

    onto z-score calculation

--- lab1_practice.R
+++ lab1_practice.R
@@ -50,4 +50,39 @@ female.summary <- sum.func(data, "female")
 male15.summary <- sum15.func(data, "male")
 female15.summary <- sum15.func(data, "female")
 
-#create a function that splits the data by age and sex and calculates the 5, 25, 50, 75 and 95 quantiles
+#create a function that splits the data by age and sex and calculates the 5, 25, 50, 75 and 95 quantiles for ht wt and vo2max
+
+perc.func <- function(sx, dat) {
+  x <- dat %>%
+    select(age, sex, ht, wt, vo2max) %>%
+    filter(sex == sx) %>%
+    select(!sex) %>%
+    pivot_longer(2:ncol(.),
+                 names_to = "vars",
+                 values_to = "val") %>%
+    group_by(age, vars) %>%
+    summarise(q5 = quantile(val, 
+                            prob = 0.05, 
+                            na.rm = TRUE),
+              q25 = quantile(val, 
+                             prob = 0.25, 
+                             na.rm = TRUE),
+              q50 = quantile(val, 
+                             prob = 0.50, 
+                             na.rm = TRUE),
+              q75 = quantile(val, 
+                             prob = 0.75, 
+                             na.rm = TRUE),
+              q95 = quantile(val, 
+                             prob = 0.95, 
+                             na.rm = TRUE)
+    )
+  
+  return(x)
+}
+
+male_percentiles <- perc.func("male", data)
+female_percentiles <- perc.func("female", data)
+
+#split file by sex and age and calculate the z-scores for vo2max, grpmaxc, pushups, sitreach, situps
+
